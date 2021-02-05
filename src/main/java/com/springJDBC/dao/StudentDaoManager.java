dc22074f64c9104382ebcbb0aa2622c43f47571a
Akhilesh Garg <akgarg0472@gmail.com>
2021-02-05 14:30:46 +0530

    Updated DAO implementation of Student class

--- src/main/java/com/springJDBC/dao/StudentDaoManager.java
+++ src/main/java/com/springJDBC/dao/StudentDaoManager.java
@@ -3,7 +3,7 @@ package com.springJDBC.dao;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class StudentDaoManager implements StudentDAO {
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public StudentDaoManager(JdbcTemplate template) {
         this.jdbcTemplate = template;
@@ -12,26 +12,22 @@ public class StudentDaoManager implements StudentDAO {
     @Override
     public int insert(int id, String name, String address) {
         String query = "INSERT INTO student (id, name, address) VALUES(?,?,?)";
-        int queryResult = this.jdbcTemplate.update(query, id, name, address);
-        return queryResult;
+        return this.jdbcTemplate.update(query, id, name, address);
     }
 
     @Override
     public int insert(Student student) {
-        int queryResult = this.insert(student.getId(), student.getName(), student.getAddress());
-        return queryResult;
+        return this.insert(student.getId(), student.getName(), student.getAddress());
     }
 
     @Override
     public int delete(int id) {
         String query = "DELETE FROM student WHERE id=(?)";
-        int queryResult = this.jdbcTemplate.update(query, id);
-        return queryResult;
+        return this.jdbcTemplate.update(query, id);
     }
 
     @Override
     public int delete(Student student) {
-        int queryResult = this.delete(student.getId());
-        return queryResult;
+        return this.delete(student.getId());
     }
 }
